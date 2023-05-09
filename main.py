413f06bb4412f50c6444500d359f75e215969db5
khaleb001 <caleb.benjamin@binghamuni.edu.ng>
2023-05-09 15:28:50 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+ # This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.class Mathematics:
+class Mathematics:
+     def addition(self, a, b):
+         return a + b
+     def subtraction(self, a, b):
+         return a - b
+     def multiplication(self, a, b):
+         return a * b
+     def division(self, a, b):
+         return a / b
+     def exponentiation(self, a, b):
+         return a ** b
+
