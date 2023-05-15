9d6ede8c6eaaf1c1576994038211940fc5a18cdb
khaleb001 <caleb.benjamin@binghamuni.edu.ng>
2023-05-15 14:46:49 +0100

    largest number in a list

--- main.py
+++ main.py
@@ -2,15 +2,17 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.class Mathematics:
-class Mathematics:
-     def addition(self, a, b):
-         return a + b
-     def subtraction(self, a, b):
-         return a - b
-     def multiplication(self, a, b):
-         return a * b
-     def division(self, a, b):
-         return a / b
-     def exponentiation(self, a, b):
-         return a ** b
+#largest element in list
 
+#function
+def largest(list):
+    large= list[0]
+    for i in list:
+        if i>large:
+            large=i
+    return large
+
+#list
+list=[3, 9, 7, 3, 6, 5, 7, 24, 6]
+print("largest in ",list,"is")
+print(largest(list))
