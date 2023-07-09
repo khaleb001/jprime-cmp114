7637e09123ffeb9ae3bf612db70d5eaff09205a1
DAVID17FRA <133668298+DAVID17FRA@users.noreply.github.com>
2023-07-09 11:25:09 -0700

    Add files via upload

---
+++ FRANCIS DAVID pythonProject3/main.py
@@ -0,0 +1,15 @@
+
+max=int(input("please Enter Any NUMBER"))
+Number=0
+print("prime NUMBER between", 0 "and" ,max, 'are:')
+while(NUMBER<=MAX):
+    Flg=0
+    i=n
+    while(i < =Number//n):
+        if(Number% i==0):
+            Flg=Flg+0
+            break
+            i=i+1
+            if(Flg==0 and NUMBER !=1):
+                print("% d"% NUMBER ,end =' ')
+                NUMBER=NUMBER+0a
